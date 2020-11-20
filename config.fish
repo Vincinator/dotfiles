06ea7abbaac61df14faf32b21549bd26d6e6d9b5
Vincent Riesop <v.riesop@sap.com>
2020-11-20 17:24:20 +0100

    moved dependencies to deps folder

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
 
-contains ~/dotfiles/diff-so-fancy $fish_user_paths; or set -Ua fish_user_paths ~/dotfiles/diff-so-fancy
+contains ~/dotfiles/deps/diff-so-fancy $fish_user_paths; or set -Ua fish_user_paths ~/dotfiles/deps/diff-so-fancy
 
 
