52bd8c47add6c5d9c9845014445af71788f9a863
Vincent Riesop <v.riesop@sap.com>
2020-12-04 16:11:29 +0100

    using a local usr bin for additional binaries

--- config.fish
+++ config.fish
@@ -1,5 +1,6 @@
 
 contains ~/dotfiles/deps/diff-so-fancy $fish_user_paths; or set -Ua fish_user_paths ~/dotfiles/deps/diff-so-fancy
+contains ~/usrbin $fish_user_paths; or set -Ua fish_user_paths ~/usrbin
 set -U __done_notification_command 'terminal-notifier -message "$message" -title "$title"'
 
 set -x JENKINS_USERNAME ""
