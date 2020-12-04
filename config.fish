a741e1f1907e484f30dffb7413c9e348c97175bb
Vincent Riesop <vincent.riesop@gmail.com>
2020-12-04 15:14:04 +0000

    added fisher

--- config.fish
+++ config.fish
@@ -3,7 +3,7 @@ contains ~/dotfiles/deps/diff-so-fancy $fish_user_paths; or set -Ua fish_user_pa
 contains ~/usrbin $fish_user_paths; or set -Ua fish_user_paths ~/usrbin
 set -U __done_notification_command 'terminal-notifier -message "$message" -title "$title"'
 
-set -x JENKINS_USERNAME ""
+set -x JENKINS_USERNAME "cerebro"
 set -x JENKINS_URL "http://10.125.1.120:8080"
 set -x JENKINS_SECRET ""
 
